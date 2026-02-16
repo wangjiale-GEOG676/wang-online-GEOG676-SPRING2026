32a2ed43769b45115dab81b8abc056575de874cd
wangjiale-GEOG676 <wangjiale@tamu.edu>
2026-02-16 14:42:52 -0600

    lab4 finished

--- lab_4/codes_env/lab4.py
+++ lab_4/codes_env/lab4.py
@@ -1,10 +1,12 @@
 # create a gdb and garage feature
 import arcpy
 
-# Workspace 指向 codes_env
+arcpy.env.overwriteOutput = True
+
+# Workspace point to codes_env
 arcpy.env.workspace = r'D:\博士资料\26 spring\GEOG675\wang-online-GEOG676-SPRING2026\lab_4\codes_env'
 
-# Lab4 根目录
+# Lab4 root
 folder_path = r'D:\博士资料\26 spring\GEOG675\wang-online-GEOG676-SPRING2026\lab_4'
 gdb_name = 'Test.gdb'
 gdb_path = folder_path + '\\' + gdb_name
@@ -15,9 +17,13 @@ arcpy.CreateFileGDB_management(folder_path, gdb_name)
 # 输入 CSV
 csv_path = r'D:\博士资料\26 spring\GEOG675\wang-online-GEOG676-SPRING2026\lab_4\data\04\garages.csv'
 garage_layer_name = 'Garage_Points'
-garages = arcpy.MakeXYEventLayer_management(csv_path, 'X', 'Y', garage_layer_name)
 
-# 写入 GDB
+garages = arcpy.MakeXYEventLayer_management(
+    csv_path, 'X', 'Y', garage_layer_name,
+    arcpy.SpatialReference(4326)  # WGS84
+)
+
+# write GDB
 input_layer = garages
 arcpy.FeatureClassToGeodatabase_conversion(input_layer, gdb_path)
 garage_points = gdb_path + '\\' + garage_layer_name
@@ -25,7 +31,7 @@ garage_points = gdb_path + '\\' + garage_layer_name
 
 # open campus gdb, copy building feature to our gdb
 campus = r'D:\博士资料\26 spring\GEOG675\wang-online-GEOG676-SPRING2026\lab_4\data\04\Campus.gdb'
-buildings_campus = campus + '\\Structures'   # ⚠ 确认里面确实叫 Structures
+buildings_campus = campus + '\\Structures'   
 buildings = gdb_path + '\\' + 'Buildings'
 
 arcpy.Copy_management(buildings_campus, buildings)
@@ -44,7 +50,8 @@ arcpy.Project_management(
 garageBuffered = arcpy.Buffer_analysis(
     gdb_path + '\\Garage_Points_reprojected',
     gdb_path + '\\Garage_Points_buffered',
-    150
+    # meters
+    "150 Meters"
 )
 
 
@@ -55,9 +62,11 @@ arcpy.Intersect_analysis(
     'ALL'
 )
 
-# 导出 CSV 到 lab_4 根目录
+# output
 arcpy.TableToTable_conversion(
-    gdb_path + '\\Garage_Building_Intersection.dbf',
+    gdb_path + '\\Garage_Building_Intersection',
     r'D:\博士资料\26 spring\GEOG675\wang-online-GEOG676-SPRING2026\lab_4',
     'nearbyBuildings.csv'
 )
+
+print("Done. Output:", folder_path + '\\nearbyBuildings.csv')
